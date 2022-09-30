237025f27da5ed1a1dbac27c09a1202b651debe1
RenanArantes <malevo18@gmail.com>
2022-09-30 16:41:13 -0300

    Summary Style Component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../../Components/Header'
+import { Summary } from '../../Components/Summary'
 
 export function Transations() {
   return (
     <div>
       <Header />
+      <Summary />
       <h1>Transactions</h1>
     </div>
   )
