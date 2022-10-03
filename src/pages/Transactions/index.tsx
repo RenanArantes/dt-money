542a572d02b0089e82e96b9ee9e891c1aaae9969
RenanArantes <malevo18@gmail.com>
2022-10-03 09:22:24 -0300

    Transactions Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,38 @@
 import { Header } from '../../Components/Header'
 import { Summary } from '../../Components/Summary'
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransationsTable,
+} from './styles'
 
 export function Transations() {
   return (
     <div>
       <Header />
       <Summary />
-      <h1>Transactions</h1>
+      <TransactionContainer>
+        <TransationsTable>
+          <tbody>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 5000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>10/10/2022</td>
+            </tr>
+            <tr>
+              <td width={'50%'}>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 5000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>10/10/2022</td>
+            </tr>
+          </tbody>
+        </TransationsTable>
+      </TransactionContainer>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // se usar a flag --host permite acessar o app por outros dispositivos conectados na mesma rede
+  },
 })
